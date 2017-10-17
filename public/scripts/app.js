3b069601da06754378edc7ff7d1c13dd979f2ce0
Rajendrasinh Parmar <rajendra20791@gmail.com>
2017-10-17 23:37:16 +0530

    Basic react without jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
 console.log("Jay Mataji");
+
+// var template = <h1>Hello This is React App with JSX</h1>;
+var appRoot = React.createElement(
+	'h1',
+	{ id: "newId"},
+	"This is React App"
+);
+
+ReactDOM.render(appRoot, document.getElementById('app'));

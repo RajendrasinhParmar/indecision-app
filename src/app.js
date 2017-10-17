f8abf5a178b5b16e562f0f2a3fd80cfe5d8ab646
Rajendrasinh Parmar <rajendra20791@gmail.com>
2017-10-18 00:19:52 +0530

    Setup babel

---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log("Jay Mataji");
+
+var template = <h1>Hello This is React App with JSX, dose this change?</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);

6431d3b5ebde2f0e5387be37d7c6001af8c9ca3e
Rajendrasinh Parmar <rajendra20791@gmail.com>
2017-10-18 09:29:29 +0530

    Add basic JSX in project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 console.log("Jay Mataji");
 
-var template = <h1>Hello This is React App with JSX, dose this change?</h1>;
+var template = (
+	<div>
+		<h1>Hello This is React App with JSX</h1>
+		<p>test</p>
+		<ol>
+			<li>Item 1</li>
+			<li>Item 2</li>
+		</ol>
+	</div>
+);
+
+var templateTwo = (
+	<div>
+		<h1>Rajendrasinh Parmar</h1>
+		<p>Age: 27</p>
+		<p>Location: Vadodara</p>
+	</div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);

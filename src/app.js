e14c19f4cefffc3d4dfb99a8c80cee2aa35fc5f0
Rajendrasinh Parmar <rajendra20791@gmail.com>
2017-11-05 12:52:02 +0530

    Add expression in jsx for dynamic data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 console.log("Jay Mataji");
 
+var app = {
+	title : "Indecision App",
+	subtitle : "This is subtitle of indecision app"
+}
+
 var template = (
 	<div>
-		<h1>Hello This is React App with JSX</h1>
-		<p>test</p>
+		<h1>{app.title}</h1>
+		<p>{app.subtitle}</p>
 		<ol>
 			<li>Item 1</li>
 			<li>Item 2</li>
@@ -11,13 +16,18 @@ var template = (
 	</div>
 );
 
+var user = {
+	name : "Rajendrasinh Parmar",
+	age : 26,
+	location : "Vadodara"
+}
 var templateTwo = (
 	<div>
-		<h1>Rajendrasinh Parmar</h1>
-		<p>Age: 27</p>
-		<p>Location: Vadodara</p>
+		<h1>{user.name}</h1>
+		<p>Age: {user.age}</p>
+		<p>Location: {user.location}</p>
 	</div>
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);

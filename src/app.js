120a0855fc8315fa52d7c323d480e9b67f078500
Rajendrasinh Parmar <rajendra20791@gmail.com>
2017-11-21 00:23:19 +0530

    Conditional rendering

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ console.log("Jay Mataji");
 
 var app = {
 	title : "Indecision App",
-	subtitle : "This is subtitle of indecision app"
+	subtitle : "This is subtitle of indecision app",
+	options: ['One', 'Two']
 }
 
 var template = (
 	<div>
 		<h1>{app.title}</h1>
-		<p>{app.subtitle}</p>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		<p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
 		<ol>
 			<li>Item 1</li>
 			<li>Item 2</li>
@@ -21,11 +23,18 @@ var user = {
 	age : 26,
 	location : "Vadodara"
 }
+function getLocation(location){
+	if (location) {
+		return <p>Location: {location}</p>;
+	} else {
+		return undefined;
+	}
+}
 var templateTwo = (
 	<div>
-		<h1>{user.name}</h1>
-		<p>Age: {user.age}</p>
-		<p>Location: {user.location}</p>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+		{ user.age >= 18 && <p>Age: {user.age}</p>}
+		{getLocation(user.location)}
 	</div>
 );
 var appRoot = document.getElementById('app');
